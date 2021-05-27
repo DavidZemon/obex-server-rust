23e137d41ddc9a53e7f64c2299c9600b8f354e0b
David Zemon <david@zemon.name>
2021-05-27 13:51:27 -0500

    Implement basic skeleton for /api/tree (hardcoded JSON response)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+use crate::EntryType::FILE;
+use serde::Serialize;
+use std::path::PathBuf;
+
+#[derive(Serialize)]
+enum EntryType {
+    FILE,
+    SYMLINK,
+    FOLDER,
+}
+
+#[derive(Serialize)]
+struct TreeEntry {
+    name: String,
+    full_path: String,
+    entry_type: EntryType,
+    size: Option<u64>,
+    children: Option<Vec<TreeEntry>>,
+    target: Option<String>,
+}
+
+#[get("/api/tree/<root..>")]
+fn tree(root: PathBuf) -> rocket_contrib::json::Json<Vec<TreeEntry>> {
+    rocket_contrib::json::Json(vec![TreeEntry {
+        name: String::from("foo"),
+        full_path: String::from(root.to_str().unwrap_or("")),
+        entry_type: FILE,
+        size: Option::None,
+        children: Option::None,
+        target: Option::None,
+    }])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![tree]).launch();
 }
